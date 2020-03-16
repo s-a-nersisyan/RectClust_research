7db3bcf250ebb90148a2a811074f3cdea7dd93c0
s-a-nersisyan <s.a.nersisyan@gmail.com>
2020-03-16 14:00:49 +0300

    More logic on algorithm core

--- MixtureEM.py
+++ MixtureEM.py
@@ -7,14 +7,36 @@ class MixtureEM:
         Abstract class implementing EM algorithm for
         the problem of mixture separation.
         '''
+        self.n_clusters = n_clusters
+        self.n_init = n_init
+        self.max_iter = max_iter
+        self.tol = tol
 
-    def fit(self, X)
+    def fit(self, X):
+        '''
+        Fit mixture model via EM algorithm given matrix X
+        '''
+        # TODO: scaling?
+        self.X = X
+        self.n_samples, self.dim = self.X.shape
+
         for init_idx in range(n_init):
             # TODO: generate initial parameters
             l_arg_min, l_min = self.run_EM()
 
     def run_EM(self):
-        pass
+        l_prev_min = None
+        for iter_idx in range(max_iter):
+            r = self.E_step()
+            l_cur_arg_min, l_cur_min = self.M_step(r)
+
+            if l_prev_min is None:
+                l_prev_min = l_cur_min
+            else:
+                if abs(l_cur_min - l_prev_min) / abs(l_prev_min) < self.tol:
+                    break
+        # TODO: raise warning if tolerance is not achieved in max_iter iterations
+        return l_cur_arg_min, l_cur_min
 
     def E_step(self):
         pass
